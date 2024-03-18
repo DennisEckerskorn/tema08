09992952a6bc6e8b9a3e49c84f4a3e190cabc0c8
Dennis Eckerskorn <denniseckerskorn@gmail.com>
2024-03-18 16:12:16 +0100

    Added imc calculation method and working on enum

--- src/main/java/com/denniseckerskorn/ejercicios/tema08/ejer06/Clasificacion.java
+++ src/main/java/com/denniseckerskorn/ejercicios/tema08/ejer06/Clasificacion.java
@@ -1,12 +1,14 @@
 package com.denniseckerskorn.ejercicios.tema08.ejer06;
 
 public enum Clasificacion {
-    DELGADEZ_SEVERA(),
-    DELGADEZ_MODERADA(),
-    DELGADEZ_LEVE(),
-    NORMAL(),
-    PREOBESIDAD(),
-    OBESIDAD_LEVE(),
-    OBESIDAD_MEDIA(),
-    OBESIDAD_MORBIDA();
+    DELGADEZ_SEVERA("Delgadez Severa"),
+    DELGADEZ_MODERADA("Delgadez Moderada"),
+    DELGADEZ_LEVE("Delgadez Leve"),
+    NORMAL("Normal"),
+    PREOBESIDAD("Preobesidad"),
+    OBESIDAD_LEVE("Obesidad Leve"),
+    OBESIDAD_MEDIA("Obesidad Media"),
+    OBESIDAD_MORBIDA("Obesidad Morbida");
 }
+
+public Clasificacion()
--- src/main/java/com/denniseckerskorn/ejercicios/tema08/ejer06/IMC.java
+++ src/main/java/com/denniseckerskorn/ejercicios/tema08/ejer06/IMC.java
@@ -2,4 +2,8 @@ package com.denniseckerskorn.ejercicios.tema08.ejer06;
 
 public class IMC {
 
+    public static float calcularIMC(float kg, float cm) {
+        float alturaMetros = cm /100;
+        return kg / (alturaMetros * alturaMetros);
+    }
 }
