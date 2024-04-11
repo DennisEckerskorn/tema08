ca7edce11d4c800ecb8209c41ba5c67c1bbf69e4
Dennis <denniseckerskorn@gmail.com>
2024-04-11 15:09:44 +0200

    Add method

--- src/main/java/com/denniseckerskorn/ejercicios/graphics/GameOfLife2.java
+++ src/main/java/com/denniseckerskorn/ejercicios/graphics/GameOfLife2.java
@@ -8,7 +8,6 @@ public class GameOfLife2 {
     private final int cellSize;
     private int[][] world;
     private int[][] nextWorld;
-    //matrix of cells
 
     public GameOfLife2(int width, int height, int cellSize) {
         this.width = width;
@@ -16,6 +15,15 @@ public class GameOfLife2 {
         this.cellSize = cellSize;
         world = new int[height][width];
         nextWorld = new int[height][width];
+        initializeMatrix(world);
+    }
+
+    private void initializeMatrix(int[][] currentWorld) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+
+            }
+        }
     }
 
     public int getWidth() {
@@ -36,12 +44,13 @@ public class GameOfLife2 {
 
     public void draw(Graphics2D g2) {
         g2.setColor(Color.BLACK);
-        for(int i = 0; i < height; i+=cellSize){
+        for (int i = 0; i < height; i += cellSize) {
             g2.drawLine(0, i, width, i);
         }
 
-        for(int i = 0; i < width; i+=cellSize) {
+        for (int i = 0; i < width; i += cellSize) {
             g2.drawLine(i, 0, i, height);
         }
+
     }
 }
